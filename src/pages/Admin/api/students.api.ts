03e9a0c3fbd2e0ace66cdeb94ac2993a8ae31819
Muha11ad <abdushukurovmukhammadjon99@gmail.com>
2024-09-30 18:27:55 +0500

    Types of axios downloaded

--- src/pages/Admin/api/students.api.ts
+++ src/pages/Admin/api/students.api.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 const studentsApi = "http://localhost:3000";
 
 const students = {
-  getStudents: () => {
-    return axios.get(`${studentsApi}/students`);
-  },
-  getSingle: (id: number) => {
-    return axios.get(`${studentsApi}/students/${id}`);
-  },
+	getStudents: () => {
+		return axios.get(`${studentsApi}/students`);
+	},
+	getSingle: (id: number) => {
+		return axios.get(`${studentsApi}/students/${id}`);
+	},
 };
 export default students;
