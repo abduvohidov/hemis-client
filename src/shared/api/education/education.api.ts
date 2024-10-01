0f411c60309e3c5875b3c7cf95f9d6ec003cbd11
Muha11ad <abdushukurovmukhammadjon99@gmail.com>
2024-10-01 16:55:07 +0500

    Education done

---
+++ src/shared/api/education/education.api.ts
@@ -0,0 +1,33 @@
+import { baseApi } from "../base.api.js";
+import { IEdcationResponse, IEducation } from "./education.types";
+export const educationApi = {
+	create: async (data: IEducation): Promise<IEdcationResponse> => {
+		return await baseApi.post<IEdcationResponse, IEducation>(
+			"/educations/create",
+			data
+		);
+	},
+	udpate: async (
+		id: number,
+		data: Partial<IEducation>
+	): Promise<IEdcationResponse> => {
+		return await baseApi.post<IEdcationResponse, Partial<IEducation>>(
+			`/educations/update/${id}`,
+			data
+		);
+	},
+	delete: async (id: number): Promise<IEdcationResponse> => {
+		return await baseApi.delete<IEdcationResponse>(`/educations/delete/${id}`);
+	},
+
+	// gets
+	getById: async (id: number): Promise<IEdcationResponse> => {
+		return await baseApi.get<IEdcationResponse>(`/educations/${id}`);
+	},
+	filter: async (data: Partial<IEducation>): Promise<IEdcationResponse[]> => {
+		return await baseApi.post<IEdcationResponse, Partial<IEducation>>(
+			`/educations/filter`,
+			data
+		);
+	},
+};
